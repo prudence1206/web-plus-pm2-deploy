b69c790099e81ba4601a16d31134c0c08f5e9071
prudence12 <prudence12@mail.ru>
2026-01-29 12:47:28 +0300

    init

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF = 'origin/master',
+} = process.env;
+module.exports = {
+  apps : [
+    {
+    name   : "name",
+    script : "dist/app.js"
+  }
+],
+ deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env',
+    },
+  },
+}
